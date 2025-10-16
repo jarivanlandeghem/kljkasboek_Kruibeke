d5654ead02cd4b78db351eb5d34b00b6cb18ca9a
Aykon Kirhan <aykon.kirhan@student.hogent.be>
2025-10-16 21:35:02 +0200

    Add routing and new pages for categories, transactions, users, and about sections
    + faker@js toegevoegd

--- kirhanhuyghe-frontend/src/main.jsx
+++ kirhanhuyghe-frontend/src/main.jsx
@@ -1,10 +1,26 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import App from './App.jsx';
+import { createBrowserRouter } from 'react-router';
+import { RouterProvider } from 'react-router/dom';
+import CategoriesPage from './pages/CategoriesPage.jsx';
+import TransactionsPage from './pages/TransactionsPage.jsx';
+import About from './pages/about/About.jsx'
+import NotFound from './pages/NotFound.jsx';
+import UsersPage from './pages/UsersPage.jsx'
+
+const router = createBrowserRouter([
+  {path: '/',Component: App,},
+  {path: 'categories', Component:CategoriesPage,},
+  {path: 'transactions',Component:TransactionsPage,},
+  {path: 'about',Component:About,},
+  {path:'users',Component:UsersPage,},
+  {path:'*',Component:NotFound,},
+]);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
---
+++ kirhanhuyghe-frontend/src/pages/NotFound.jsx
@@ -0,0 +1,10 @@
+const NotFound = () => {
+  return (
+    <div>
+      <h1 className="text-4xl mb-4">Pagina niet gevonden</h1>
+      <p>Er is geen pagina op deze url, probeer iets anders.</p>
+    </div>
+  );
+};
+
+export default NotFound;
