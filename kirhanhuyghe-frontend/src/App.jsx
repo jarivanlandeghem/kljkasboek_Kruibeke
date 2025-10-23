2a77e3f6b0dec22e23f6600adf89ec825e5750cf
Aykon Kirhan <aykon.kirhan@student.hogent.be>
2025-10-23 16:12:46 +0200

    Add react-router-dom dependency, refactor routing, and update NotFound page with navigation

--- kirhanhuyghe-frontend/src/App.jsx
+++ kirhanhuyghe-frontend/src/App.jsx
@@ -1,14 +1,14 @@
-import HomePage from "./pages/HomePage";
+import { Outlet } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 function App() {  
   const username="Aykon" 
-  {/*later dit uit database halen */}
+  // later dit uit database halen
   return(
     <div>
       <Navbar username={username}/>
       <main className="pt-20">
-        <HomePage/>
+        <Outlet />
       </main>
       <Footer/>
     </div>
---
+++ kirhanhuyghe-frontend/src/components/transactions/TransactionTable.jsx
@@ -0,0 +1,7 @@
+export default function TransactionTable(){
+  return(
+    <div>
+      <h1>test</h1>
+    </div>
+  );
+}
