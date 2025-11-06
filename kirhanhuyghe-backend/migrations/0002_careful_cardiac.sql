3aeeaa0f907b3c35897ba1d196b1c9a8a0fd5535
Aykon Kirhan <aykon.kirhan@student.hogent.be>
2025-11-06 14:47:09 +0100

    layout fixes

--- kirhanhuyghe-backend/migrations/0002_careful_cardiac.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE `categories` (
-	`categorieID` int AUTO_INCREMENT NOT NULL,
-	`naam` varchar(255) NOT NULL,
-	`type` varchar(255) NOT NULL,
-	CONSTRAINT `categories_categorieID` PRIMARY KEY(`categorieID`)
-);
---> statement-breakpoint
-ALTER TABLE `transacties` MODIFY COLUMN `transactieID` int AUTO_INCREMENT NOT NULL;--> statement-breakpoint
-ALTER TABLE `transacties` MODIFY COLUMN `datum` text NOT NULL;
