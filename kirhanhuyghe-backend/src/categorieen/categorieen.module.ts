b940ebb9ff4b089f3bb20ea33ebcfd7b78eebf29
Jasper Huyghe <jasper.huyghe@student.hogent.be>
2025-11-11 23:34:18 +0100

    Adds registration and profile pages(alleen design & routing)
    Adds a registration page with role selection and validation.
    Creates a profile page for users to manage their information and change passwords.
    Fixes a module naming issue and adds a registration route.

--- kirhanhuyghe-backend/src/categorieen/categorieen.module.ts
+++ kirhanhuyghe-backend/src/categorieen/categorieen.module.ts
@@ -9,4 +9,4 @@ import { AuthModule } from '../auth/auth.module';
   providers: [CategorieenService],
   exports: [CategorieenService],
 })
-export class TransactiesModule {}
+export class CategorieenModule {}
