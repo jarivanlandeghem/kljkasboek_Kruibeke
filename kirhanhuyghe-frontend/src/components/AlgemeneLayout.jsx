04c28e5439d4968f4d645ee012a17134cbbe6378
Aykon Kirhan <aykon.kirhan@student.hogent.be>
2025-11-23 15:24:21 +0100

    Verbeterde responsiviteit en overflow-stijlen in AlgemeneLayout en index.css

--- kirhanhuyghe-frontend/src/components/AlgemeneLayout.jsx
+++ kirhanhuyghe-frontend/src/components/AlgemeneLayout.jsx
@@ -3,24 +3,32 @@ import { ScrollRestoration } from "react-router";
 
 export default function AlgemeneLayout({ image, children }) {
   return (
-    <>
+      <div className="relative w-full overflow-x-hidden">
       <Navbar />
 
-      <div className="flex flex-col md:flex-row min-h-screen pt-20">
-        <div className="md:w-1/2 w-full">
+      <div className="relative w-full">
+        <div className="hidden md:block fixed top-20 left-0 w-[50vw] h-[calc(100vh-5rem)] z-0">
           <img
             src={image}
             alt="Kinderen die spelen"
-            className="object-cover w-full h-56 md:h-auto"
+            className="object-cover w-full h-full"
           />
         </div>
 
-        <div className="md:w-1/2 w-full flex items-center justify-center bg-white p-4">
+        <div className="md:hidden w-full">
+          <img
+            src={image}
+            alt="Kinderen die spelen"
+            className="object-cover w-full h-56"
+          />
+        </div>
+
+        <div className="relative z-10 w-full md:w-[50vw] md:ml-[50vw] min-h-[calc(100vh-5rem)] flex items-center justify-center bg-white p-4">
           {children}
         </div>
 
         <ScrollRestoration />
       </div>
-    </>
+    </div>
   );
 }
